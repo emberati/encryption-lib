03c4316363e0934b806a27a2207fc56677746608
emberati <alex.shak55@gmail.com>
2023-02-12 01:45:23 +0300

    Implemented Timer class to track code execution time

--- src/main/java/com/emb/main/Main.java
+++ src/main/java/com/emb/main/Main.java
@@ -1,4 +1,82 @@
 package com.emb.main;
 
+import com.emb.util.ByteUtils;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.StringJoiner;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
 public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        var unsLong = 0x96EA704CFB1CF672L;
+        var sigLong = Long.toUnsignedString(unsLong);
+        var unsignedLongBytesString = Long.toUnsignedString(unsLong, 2);
+//        var str = new String(unsignedLongToByteArray(unsLong), StandardCharsets.UTF_16LE);
+
+//        System.out.println(unsLong);
+//        System.out.println(sigLong);
+//        System.out.println(str);
+
+        int bytes = 0b00000100;
+        var decimal = Integer.toString(bytes);
+        var binary = Integer.toBinaryString(bytes);
+        var shifted = Integer.toBinaryString(bytes >> 1);
+
+        System.out.println(decimal);
+        System.out.println(binary);
+        System.out.println(shifted);
+
+//        System.out.println(Arrays.toString(ByteUtils.unsignedLongToByteArray(0xFF)));
+        System.out.println(Long.toBinaryString(0xFF));
+        System.out.println(ByteUtils.makePrettyBinaryString(0b1101));
+        System.out.println(ByteUtils.makePrettyBinaryString(0b101));
+
+        var times = 1000;
+        var timer = new Timer();
+
+//        System.out.println(timer.getSummary());
+
+//        var separatedLoopsMethodTime = benchmark(Main::makePrettyBinaryStringSeparatedLoops, times);
+//        var combinedLoopsMethodTime = benchmark(Main::makePrettyBinaryStringCombinedLoops, times);
+        timer.bench(Main::makePrettyBinaryStringCombinedLoops, times);
+        System.out.println(timer.getSummary());
+        timer.bench(Main::makePrettyBinaryStringSeparatedLoops, times);
+        System.out.println(timer.getSummary());
+    }
+
+    public static String makePrettyBinaryStringSeparatedLoops() {
+        return ByteUtils.makePrettyBinaryString0(0x96EA704CFB1CF672L, 2);
+    }
+
+    public static String makePrettyBinaryStringCombinedLoops() {
+        return ByteUtils.makePrettyBinaryString(0x96EA704CFB1CF672L, 2);
+    }
+
+    public static <T> long benchmark(Supplier<T> function, int times) {
+        var startTime = 0L;
+        var endTime = 0L;
+        var totalTime = 0L;
+        var averageTime = 0L;
+
+        final T result = function.get();
+
+        for (int i = 0; i < times; i++) {
+            startTime = System.nanoTime();
+            function.get();
+            endTime = System.nanoTime();
+            totalTime += endTime - startTime;
+        }
+
+        averageTime = totalTime / times;
+
+        System.out.printf("Result: %s%n", result.toString());
+        System.out.printf("Estimated (%s times): %d %f (sec) %n", times, totalTime, (double) totalTime / 1000 / 1000 / 1000);
+        System.out.printf("Estimated in average: %d %f (sec) %n%n", averageTime, (double) averageTime / 1000 / 1000 / 1000);
+
+        return totalTime;
+    }
 }
---
+++ src/main/java/com/emb/main/Timer.java
@@ -0,0 +1,138 @@
+package com.emb.main;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Supplier;
+
+public class Timer {
+    private final List<Long> periods = new LinkedList<>();
+    private long startTime = 0L;
+    private long minTime = Long.MAX_VALUE;
+    private long maxTime = 0L;
+    private String summary;
+//    private String lastPeriodName = null;
+
+    public Timer() {
+        reset();
+    }
+
+    public long click() {
+        if (isRunning()) return stop();
+
+        start();
+        return 0L;
+    }
+
+    public void start() {
+//        lastPeriodName = periodName;
+//        periods.put(lastPeriodName, null);
+        summary = null;
+        startTime = System.nanoTime();
+    }
+
+    public long stop() {
+        var estimated = getCurrentTime();
+        startTime = 0L;
+//        periods.put(lastPeriodName, estimated);
+        if (estimated < minTime) minTime = estimated;
+        if (estimated > maxTime) maxTime = estimated;
+        periods.add(estimated);
+        return estimated;
+    }
+
+    public void reset() {
+        startTime = 0L;
+        periods.clear();
+        summary = null;
+    }
+
+    public long getPeriodTime(int period) {
+        return periods.get(period);
+    }
+
+    public long getLastTime() {
+        return getPeriodTime(periods.size() - 1);
+    }
+
+    public long getCurrentTime() {
+        if (isRunning())
+            return System.nanoTime() - startTime;
+        else return 0L;
+    }
+
+    public long getTotalTime() {
+        var totalTime = 0L;
+        for (var time : periods) {
+            totalTime += time;
+        }
+        return totalTime;
+    }
+
+    public double getAverageTime() {
+        return (double) getTotalTime() / periods.size();
+    }
+
+    public long getMinTime() {
+        return minTime;
+    }
+
+    public long getMaxTime() {
+        return maxTime;
+    }
+
+    public int getPeriodsAmount() {
+        return periods.size();
+    }
+
+    public String getSummary() {
+        return """
+               Estimated total: %dns
+               Estimated average: %fns
+               Estimated min, max: %dns, %dns
+               %d periods total (in ns): %s
+               """.formatted(getTotalTime(),
+                             getAverageTime(),
+                             getMinTime(),
+                             getMaxTime(),
+                             getPeriodsAmount(),
+                             periods.toString());
+    }
+
+    public <T> long bench(Supplier<T> function) {
+        return bench(function, 1);
+    }
+
+    public <T> long bench(Supplier<T> function, int times) {
+        final T result = function.get();
+        reset();
+
+        for (int i = 0; i < times; i++) {
+            start();
+            function.get();
+            stop();
+        }
+
+        summary = Optional.ofNullable(summary)
+                .orElse("""
+                    Result: %s
+                    Estimated (%s times): %d %f (sec)
+                    Estimated in average: %f %f (sec)
+                    """.formatted(
+                            result.toString(),
+                            times, getTotalTime(), (double) getTotalTime() / 1000 / 1000 / 1000,
+                            getAverageTime(), getAverageTime() / 1000 / 1000 / 1000));
+
+        return getTotalTime();
+    }
+
+    public boolean isRunning() {
+        return startTime != 0L;
+    }
+
+    @Override
+    public String toString() {
+        return "Timer(isRunning=%s, currentTime=%d, lastTime=%d, periodsAmount=%d, min=%d, max=%d)"
+                .formatted(isRunning(), getCurrentTime(), getLastTime(), periods.size(), getMinTime(), getMaxTime());
+    }
+}
