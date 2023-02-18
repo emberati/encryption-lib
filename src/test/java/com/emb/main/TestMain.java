460d5eb4f7eda06ab3e5cbea10a38c21ed94c889
emberati <alex.shak55@gmail.com>
2023-02-18 23:31:29 +0300

    Implemented some unit tests, added JUnit dependency

---
+++ src/test/java/com/emb/main/TestMain.java
@@ -0,0 +1,59 @@
+package com.emb.main;
+
+import com.emb.util.ByteUtils;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+public class TestMain {
+    private static final Charset charset = StandardCharsets.UTF_8;
+    private static final String message = "Fuck u nigga!";
+    private static final byte[] messageBytes = message.getBytes(charset);
+
+    @Test
+    public void testMessageEncryptDecrypt() {
+        var encryptedMessageBytes = FeistelEncrypt.encrypt(message.getBytes(charset));
+        var decryptedMessageBytes = FeistelEncrypt.decrypt(encryptedMessageBytes);
+
+        var encryptedMessage = new String(encryptedMessageBytes, charset);
+        var decryptedMessage = new String(decryptedMessageBytes, charset);
+
+//        System.out.printf("Original bytes:%n%s%n", joinPrettyBytes(messageBytes));
+//        System.out.printf("Original stroke:%n%s%n", message);
+//        System.out.printf("Encrypted bytes:%n%s%n", joinPrettyBytes(encryptedMessageBytes));
+//        System.out.printf("Encrypted stroke:%n%s%n", encryptedMessage);
+//        System.out.printf("Decrypted bytes:%n%s%n", joinPrettyBytes(decryptedMessageBytes));
+//        System.out.printf("Decrypted stroke:%n%s%n", decryptedMessage);
+
+        Assert.assertEquals(message, decryptedMessage);
+//        Assert.assertEquals(messageBytes, decryptedMessageBytes);
+    }
+
+    @Test
+    public void testLongEncryptDecrypt() {
+        final var longValue = 0x1AFE7292F5DDAE22L;
+        final var encryptedBytesLong = FeistelEncrypt.encryptBlock(longValue);
+        final var decryptedByteLong = FeistelEncrypt.decryptBlock(encryptedBytesLong);
+        final var originalBytesString = ByteUtils.numberToPrettyBinaryString(longValue, 16);
+        final var decryptedBytesString = ByteUtils.numberToPrettyBinaryString(decryptedByteLong, 16);
+
+        Assert.assertEquals(originalBytesString, decryptedBytesString);
+    }
+
+    @Test
+    public void testByteArrayEncryptDecrypt() {
+        final var bytes = new byte[] {
+                (byte) 0b0000_0000, (byte) 0b0000_0001, (byte) 0b0001_1110, (byte) 0b1111_0011,
+                (byte) 0b110_00000, (byte) 0b101_00101, (byte) 0b100_10010, (byte) 0b1111_1101
+        };
+        final var condensedBytesLong = ByteUtils.bytesToLong(bytes);
+        final var encryptedBytesLong = FeistelEncrypt.encryptBlock(condensedBytesLong);
+        final var decryptedByteLong = FeistelEncrypt.decryptBlock(encryptedBytesLong);
+        final var originalBytesString = ByteUtils.numberToPrettyBinaryString(condensedBytesLong, 16);
+        final var decryptedBytesString = ByteUtils.numberToPrettyBinaryString(decryptedByteLong, 16);
+
+        Assert.assertEquals(originalBytesString, decryptedBytesString);
+    }
+}
---
+++ src/test/java/com/emb/util/TestByteUtils.java
@@ -0,0 +1,93 @@
+package com.emb.util;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+public class TestByteUtils {
+    private static final long number = 0xFA3CDEBL;
+    private static final String binaryString = Long.toBinaryString(number);
+    private final static String prettifiedBinaryString = "1111 1010 0011 1100 1101 1110 1011";
+    private final static String prettifiedBinaryStringNoMatchingZeros = "0000 0000 0000 0000 0000 0000 0000 0000 0000 1111 1010 0011 1100 1101 1110 1011";
+    private final byte[] byteArray = new byte[] {
+            0b0101_0110, 0b0110_1110, 0b0101_1111, 0b0101_0110,
+            0b0110_1101, 0b0001_0010, 0b0111_1100, 0b0000_1001
+    };
+    private final long longValue = 0b0101_0110_0110_1110_0101_1111_0101_0110_0110_1101_0001_0010_0111_1100_0000_1001L;
+
+    private final long[] transactedLongArray = new long[] {
+            0x11EF3C0A592FDL, 0x1AFE7292F5DDAE22L,
+            0x2543BB8A3CD2B23FL, 0xCD198DEB25F035C3L
+    };
+    private final byte[] transactedByteArray = new byte[] {
+            (byte) 0b0000_0000, (byte) 0b0000_0001, (byte) 0b0001_1110, (byte) 0b1111_0011,
+            (byte) 0b110_00000, (byte) 0b101_00101, (byte) 0b100_10010, (byte) 0b1111_1101,
+            (byte) 0b000_11010, (byte) 0b111_11110, (byte) 0b011_10010, (byte) 0b1001_0010,
+            (byte) 0b111_10101, (byte) 0b110_11101, (byte) 0b101_01110, (byte) 0b0010_0010,
+            (byte) 0b001_00101, (byte) 0b010_00011, (byte) 0b101_11011, (byte) 0b1000_1010,
+            (byte) 0b001_11100, (byte) 0b110_10010, (byte) 0b101_10010, (byte) 0b0011_1111,
+            (byte) 0b110_01101, (byte) 0b000_11001, (byte) 0b100_01101, (byte) 0b1110_1011,
+            (byte) 0b001_00101, (byte) 0b111_10000, (byte) 0b001_10101, (byte) 0b1100_0011
+    };
+
+    @Test
+    public void testPrettifiedBackwardCapability() {
+        final var prettifiedBinaryString = ByteUtils.prettifyBinaryString(binaryString, 16);
+        final var uglifiedBinaryString = removeSpaces(prettifiedBinaryString);
+
+        try {
+            Assert.assertEquals(Long.parseLong(uglifiedBinaryString, 2), number);
+        } catch (Exception e) {
+            Assert.fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void testPrettifyBinaryString() {
+        final var prettifiedBinaryString = ByteUtils.prettifyBinaryString(binaryString);
+        Assert.assertEquals(TestByteUtils.prettifiedBinaryString, prettifiedBinaryString);
+    }
+
+    @Test
+    public void testPrettifyBinaryStringNoMatchingZeros() {
+        final var prettifiedBinaryString = ByteUtils.prettifyBinaryString(binaryString, 16);
+        Assert.assertEquals(prettifiedBinaryStringNoMatchingZeros, prettifiedBinaryString);
+    }
+
+    @Test
+    public void testByteArrayToLongTranslation() {
+        Assert.assertEquals(longValue, ByteUtils.bytesToLong(byteArray));
+    }
+
+    @Test
+    public void testLongToByteArrayTranslation() {
+        final var translatedBytes = ByteUtils.longToBytes(longValue);
+
+        Assert.assertEquals(Arrays.toString(byteArray), Arrays.toString(translatedBytes));
+    }
+
+    @Test
+    public void testByteArrayToLongArray() {
+        final var translatedLongArray = ByteUtils.byteArrayToLongArray(transactedByteArray);
+
+        System.out.println(Arrays.toString(transactedLongArray));
+        System.out.println(Arrays.toString(translatedLongArray));
+
+        Assert.assertEquals(Arrays.toString(transactedLongArray), Arrays.toString(translatedLongArray));
+    }
+
+    @Test
+    public void testLongArrayToBytesArray() {
+        final var translatedByteArray = ByteUtils.longArrayToByteArray(transactedLongArray);
+
+        System.out.println(Arrays.toString(transactedByteArray));
+        System.out.println(Arrays.toString(translatedByteArray));
+
+        Assert.assertEquals(Arrays.toString(transactedByteArray), Arrays.toString(translatedByteArray));
+    }
+
+    private String removeSpaces(final String string) {
+        return string.replace(" ", "");
+    }
+}
