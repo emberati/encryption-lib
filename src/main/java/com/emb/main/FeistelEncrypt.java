e7e0d2d1895da4a716a20f9deb402e955af30fda
emberati <alex.shak55@gmail.com>
2023-03-08 16:15:38 +0300

    Fixed bug with no mathing zeros in last long in `encrypt(byte[])`, `decrypt(byte[])`

--- src/main/java/com/emb/main/FeistelEncrypt.java
+++ src/main/java/com/emb/main/FeistelEncrypt.java
@@ -2,6 +2,7 @@ package com.emb.main;
 
 import com.emb.util.ByteUtils;
 
+import java.util.Arrays;
 import java.util.function.Function;
 
 public class FeistelEncrypt {
@@ -129,35 +130,44 @@ public class FeistelEncrypt {
     }
 
     private static byte[] process(Function<Long, Long> action, byte[] source) {
-        final var target = new byte[Math.max(source.length, Long.BYTES)];
-        System.out.println("encrypting...");
-        // FIXME: 19.02.2023
-        var longBuffer = new byte[Long.BYTES];
+        final var target = new byte[Long.BYTES * ByteUtils.amountOfLongsInByteArray(source)];
+        System.out.printf("s: %s, t: %s, b: %s%n", source.length, target.length, ByteUtils.amountOfLongsInByteArray(source));
+
+        var buffer = new byte[Long.BYTES];
         var i = 0;
         var section = Math.min(Long.BYTES, source.length - i);
         var block = 0L;
 
         while (section > 0) {
-            longBuffer = new byte[section];
+            Arrays.fill(buffer, 0, Long.BYTES - section, (byte) 0);
 
-            System.arraycopy(source, i, longBuffer, 0, section);
-            longBuffer = ByteUtils.stretchByteArrayToLong(longBuffer);
-            block = ByteUtils.byteArrayToLong(longBuffer);
-
-            printBufferAndBlock(longBuffer, block);
+            System.arraycopy(source, i, buffer, Long.BYTES - section, section);
+            block = ByteUtils.byteArrayToLong(buffer);
 
             block = action.apply(block);
-            longBuffer = ByteUtils.longToByteArray(block);
+            buffer = ByteUtils.longToByteArray(block);
 
-            printBufferAndBlock(longBuffer, block);
-
-            System.arraycopy(longBuffer, 0, target, i, Long.BYTES);
+            System.arraycopy(buffer, 0, target, i, Long.BYTES);
 
             i += section;
             section = Math.min(Long.BYTES, source.length - i);
         }
-        System.out.println();
-        return target;
+
+        // cleaning zeros
+
+        byte[] tail;
+        byte[] head;
+
+        tail = Arrays.copyOfRange(target, target.length - Long.BYTES, target.length);
+        tail = ByteUtils.removeZeroPrefix(tail);
+        head = Arrays.copyOfRange(target, 0, target.length - Long.BYTES);
+
+        buffer = new byte[target.length - Long.BYTES + tail.length];
+
+        System.arraycopy(head, 0, buffer, 0, head.length);
+        System.arraycopy(tail, 0, buffer, head.length, tail.length);
+
+        return buffer;
     }
 
     private static byte[] processAlternate(Function<Long, Long> action, byte[] source) {
