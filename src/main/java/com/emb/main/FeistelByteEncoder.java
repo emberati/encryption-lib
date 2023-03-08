60d9f68c4d0a1e24e06273f35486eb32913c0194
emberati <alex.shak55@gmail.com>
2023-03-08 16:11:22 +0300

    Code refactoring in `FeistelByteEncoder`

--- src/main/java/com/emb/main/FeistelByteEncoder.java
+++ src/main/java/com/emb/main/FeistelByteEncoder.java
@@ -6,13 +6,10 @@ public class FeistelByteEncoder implements Encoder<byte[]> {
     @Override
     public byte[] encrypt(byte[] data) {
         return FeistelEncrypt.encrypt(data);
-//        return FeistelEncrypt.encrypt0(data);
     }
 
     @Override
     public byte[] decrypt(byte[] data) {
-//        return FeistelEncrypt.decrypt(data);
-        return ByteUtils.removeZeroPrefix(FeistelEncrypt.decrypt(data));
-//        FeistelEncrypt.decrypt0(data);
+        return FeistelEncrypt.decrypt(data);
     }
 }
