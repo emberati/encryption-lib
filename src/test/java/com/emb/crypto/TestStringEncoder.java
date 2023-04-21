a4a59d388489d0ee10cdfa8c1ed3aa710486be28
emberati <alex.shak55@gmail.com>
2023-04-22 02:12:08 +0300

    Implemented tests for `StringEncoder` with different byte encoders

--- src/test/java/com/emb/crypto/TestStringEncoder.java
+++ src/test/java/com/emb/crypto/TestStringEncoder.java
@@ -2,15 +2,32 @@ package com.emb.crypto;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ArgumentsSource;
-import org.junit.jupiter.params.provider.MethodSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestStringEncoder {
     @ParameterizedTest
     @ArgumentsSource(TestStringEncoderArgumentProvider.class)
-    public void testECBStringEncoderEncryptDecrypt(String original, String controlValue) {
-        final var byteEncoder = new StringEncoder();
+    public void testECBStringEncoder(String original, String controlValue) {
+        final var byteEncoder = new StringEncoder(new ECBByteEncoder());
+        final var encoded = byteEncoder.encode(original);
+        final var decoded = byteEncoder.decode(encoded);
+        assertEquals(controlValue, decoded);
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(TestStringEncoderArgumentProvider.class)
+    public void testCBCStringEncoder(String original, String controlValue) {
+        final var byteEncoder = new StringEncoder(new CBCByteEncoder());
+        final var encoded = byteEncoder.encode(original);
+        final var decoded = byteEncoder.decode(encoded);
+        assertEquals(controlValue, decoded);
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(TestStringEncoderArgumentProvider.class)
+    public void testOFBStringEncoder(String original, String controlValue) {
+        final var byteEncoder = new StringEncoder(new OFBByteEncoder());
         final var encoded = byteEncoder.encode(original);
         final var decoded = byteEncoder.decode(encoded);
         assertEquals(controlValue, decoded);
