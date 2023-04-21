04d6c871eaffc922878aeea2c5bd257762c0da5f
emberati <alex.shak55@gmail.com>
2023-04-21 22:29:13 +0300

    Implemented inheritance of any byte encoders based on Feistel Network from `FeistelChiper`

--- src/main/java/com/emb/crypto/ECBByteEncoder.java
+++ src/main/java/com/emb/crypto/ECBByteEncoder.java
@@ -6,71 +6,22 @@ import java.util.Arrays;
 import java.util.Random;
 import java.util.function.Function;
 
-public class ECBByteEncoder implements Encoder<byte[]> {
-
-    private final long key;
-    private final long seed;
-    private final int roundsAmount;
-
-    public ECBByteEncoder() {
-        this(new Random().nextLong(), 8);
-    }
-
-    public ECBByteEncoder(long seed, int roundsAmount) {
-        this.key = new Random(seed).nextLong();
-        this.seed = seed;
-        this.roundsAmount = roundsAmount; // TODO: Не учитывать циклические раунды
-    }
-
-    @Override
-    public byte[] encode(byte[] data) {
-        final var longArray = ByteUtils.byteArrayToLongArray(data);
-
-        for (int i = 0; i < longArray.length; i++) {
-            longArray[i] = encryptBlock(longArray[i]);
-        }
-
-        var byteArray = ByteUtils.longArrayToByteArray(longArray);
-        byteArray = ByteUtils.removeNonMatchingZeros(byteArray);
-
-        return byteArray;
-    }
-
-    @Override
-    public byte[] decode(byte[] data) {
-        final var longArray = ByteUtils.byteArrayToLongArray(data);
-
-        for (int i = 0; i < longArray.length; i++) {
-            longArray[i] = decryptBlock(longArray[i]);
-        }
-
-        var byteArray = ByteUtils.longArrayToByteArray(longArray);
-        byteArray = ByteUtils.removeNonMatchingZeros(byteArray);
-
-        return byteArray;
-    }
-
-    private long encryptBlock(long block) {
-        return FeistelCipher.encrypt(key, block, roundsAmount, false);
-    }
-
-    private long decryptBlock(long block) {
-        return FeistelCipher.encrypt(key, block, roundsAmount,true);
-    }
+public class ECBByteEncoder extends FeistelCipher<byte[]> {
 
     // Разбивание на блоки и параллельное шфрование/дешифрование
-    private byte[] convertAndEncrypt(Function<Long, Long> action, byte[] source) {
-        final var target = new byte[Long.BYTES * ByteUtils.amountOfLongsInByteArray(source)];
+    @Override
+    protected byte[] processSequence(BlockCryptographyAction action, byte[] data) {
+        final var target = new byte[Long.BYTES * ByteUtils.amountOfLongsInByteArray(data)];
 
         var buffer = new byte[Long.BYTES];
         var i = 0;
-        var section = Math.min(Long.BYTES, source.length - i);
+        var section = Math.min(Long.BYTES, data.length - i);
         var block = 0L;
 
         while (section > 0) {
             Arrays.fill(buffer, 0, Long.BYTES - section, (byte) 0);
 
-            System.arraycopy(source, i, buffer, Long.BYTES - section, section);
+            System.arraycopy(data, i, buffer, Long.BYTES - section, section);
             block = ByteUtils.byteArrayToLong(buffer);
 
             block = action.apply(block);
@@ -79,7 +30,7 @@ public class ECBByteEncoder implements Encoder<byte[]> {
             System.arraycopy(buffer, 0, target, i, Long.BYTES);
 
             i += section;
-            section = Math.min(Long.BYTES, source.length - i);
+            section = Math.min(Long.BYTES, data.length - i);
         }
 
         // cleaning zeros
@@ -87,13 +38,5 @@ public class ECBByteEncoder implements Encoder<byte[]> {
         buffer = ByteUtils.removeNonMatchingZeros(target);
 
         return buffer;
-    }
-
-    public long getKey() {
-        return key;
-    }
-
-    public long getSeed() {
-        return seed;
     }
 }
--- src/main/java/com/emb/crypto/FeistelCipher.java
+++ src/main/java/com/emb/crypto/FeistelCipher.java
@@ -3,13 +3,55 @@ package com.emb.crypto;
 import com.emb.util.ByteUtils;
 import com.emb.util.Shift;
 
-public final class FeistelCipher {
-    private FeistelCipher() {}
+import java.util.Random;
+import java.util.function.Function;
 
-    static long encrypt(long key, long block, int roundAmount, final boolean reverse) {
+public abstract class FeistelCipher<T> implements Encoder<T> {
+
+    protected interface BlockCryptographyAction extends Function<Long, Long> {}
+
+    protected final long key;
+    protected final long seed;
+    protected final int roundsAmount;
+
+    public FeistelCipher() {
+        this(new Random().nextLong(), 8);
+    }
+
+    public FeistelCipher(long seed, int roundsAmount) {
+        this.key = new Random(seed).nextLong();
+        this.seed = seed;
+        this.roundsAmount = roundsAmount;
+    }
+
+    @Override
+    public T encode(T data) {
+        return processSequence(this::encryptBlock, data);
+    }
+
+    @Override
+    public T decode(T data) {
+        return processSequence(this::decryptBlock, data);
+    }
+
+    /**
+     * Processing abstract cryptography action (encryption process or decryption) on sequence.
+     * @param action specified cryptography action (to encrypt or to decrypt);
+     * @param data what to process with cryptography action;
+     * @return result of cryptography action.
+     */
+    protected abstract T processSequence(BlockCryptographyAction action, T data);
+
+    /**
+     * Processing abstract cryptography action on a single sequence block.
+     * @param block what to process;
+     * @param reverse how to process (backward or forward);
+     * @return result of processing.
+     */
+    protected long processBlock(long block, final boolean reverse) {
         final var roundDirection = reverse ? -1 : 1;
-        final var stopIndex = reverse ? -1 : roundAmount;
-        var roundIndex = reverse ? roundAmount - 1 : 0;
+        final var stopIndex = reverse ? -1 : roundsAmount;
+        var roundIndex = reverse ? roundsAmount - 1 : 0;
 
         // Выделяем из 64 разрядного блока левую и правую части
         var leftHalfBlock = (int) ((block >> 32) & Shift.INT.mask());  // левый подблок (32 битный)
@@ -36,18 +78,47 @@ public final class FeistelCipher {
         return (long) leftHalfBlock << 32 | rightHalfBlock & Shift.INT.mask();
     }
 
+    /**
+     * A function instance of {@link BlockCryptographyAction}.
+     * Implements <strong>encryption</strong> process for the single block of sequence.
+     * @param block is a block to encrypt;
+     * @return <strong>encrypted</strong> block.
+     */
+    protected long encryptBlock(Long block) {
+        return processBlock(block, false);
+    }
+
+    /**
+     * A function instance of {@link BlockCryptographyAction}.
+     * Implements <strong>decryption</strong> process for the single block of sequence.
+     * @param block is a block to decrypt;
+     * @return <strong>decrypted</strong> block.
+     */
+    protected long decryptBlock(long block) {
+        return processBlock(block, true);
+    }
+
     // Генерация 32 разрядного ключа на i-м раунде из исходного 64-разрядного
-    private static int roundKey(int i, long key) {
+    protected int roundKey(int i, long key) {
 //        return (int) rightShift64(key, i * 8);    // циклический сдвиг на 8 бит и обрезка правых 32 бит
-        return (int) ByteUtils.shiftAll(key, Shift.BYTE.right, 8);
+        return (int) ByteUtils.shiftAll(key, Shift.BYTE.right, i * 8);
     }
 
     // Образующая функция - функция, шифрующая половину блока halfBlock ключом K_i на i-м раунде
-    private static int F(int halfBlock, int K_i) {
+    protected int F(int halfBlock, int K_i) {
 //        int f1 = leftShift32(halfBlock, 9);
 //        int f2 = rightShift32(K_i, 11) | halfBlock;
         int f1 = (int) ByteUtils.shift(halfBlock, Shift.INT.left, 9);
         int f2 = (int) ByteUtils.shift(K_i, Shift.INT.right, 11) | halfBlock;
         return f1 ^ f2;
+    }
+
+
+    public final long getKey() {
+        return key;
+    }
+
+    public final long getSeed() {
+        return seed;
     }
 }
