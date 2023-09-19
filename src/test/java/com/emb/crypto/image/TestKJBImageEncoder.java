8917dc05c51d3135e18237c14542955e99c29894
emberati <alex.shak55@gmail.com>
2023-09-19 14:17:13 +0300

    Implemented tests for `KJBImageEncoder`

---
+++ src/test/java/com/emb/crypto/image/TestKJBImageEncoder.java
@@ -0,0 +1,10 @@
+package com.emb.crypto.image;
+
+import org.junit.jupiter.api.Test;
+
+public class TestKJBImageEncoder {
+    @Test
+    public void testKJBImageEncoder() {
+        var encoder = new KJBImageEncoder();
+    }
+}
